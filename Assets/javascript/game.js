d24ca209fb8c40f0c8ace01e3156fe8944f729b4
Peter Florek <peter.florek@yahoo.com>
2018-08-19 00:24:25 -0400

    win loss working but no random puzzle

--- Assets/javascript/game.js
+++ Assets/javascript/game.js
@@ -23,8 +23,7 @@ var $losses = 0;
 function newCocktail() {
 
   // Attaches to id applied in html doc to insert random string from array  //
-  document.getElementById("cocktailText").innerHTML = puzzle;
-
+  
   for (i = 0; i < puzzle.length; i++) {
 
 
@@ -40,119 +39,122 @@ function newCocktail() {
 
     }
     document.getElementById("cocktailText").innerHTML = placeholders.join("");
-    console.log(puzzle[i]);
+    console.log(puzzle);
   }
 
 }
 
 // Define function for starting new game
-function newGame () {
-  if ($guessRemain === 0 && placeholders.join("") !== puzzle) {
+function newGame() {
 
-    $losses ++;
-    $guessRemain = 10;
-    usedLetters = [];
-    correctGuess = [];
+  $guessRemain = 10;
+  usedLetters = [];
+  correctGuess = [];
+  placeholders = [];
 
-    document.getElementById("losses").innerHTML = ("Losses: " + $losses);
-    document.getElementById("guessRemain").innerHTML = ("Ingredients left: " + $guessRemain);document.getElementById("userGuesses").innerHTML = ("Wrong Ingredients:" + usedLetters);
+  document.getElementById("guessRemain").innerHTML = ("Ingredients left: " + $guessRemain); document.getElementById("userGuesses").innerHTML = ("Wrong Ingredients:" + usedLetters);
 
-    alert("You ran out of ingredients. Try again!");
-    newCocktail();
-
-  } else if (placeholders.join("") == puzzle) {
-
-    $wins ++;
-    $guessRemain = 10;
-    usedLetters = [];
-    correctGuess = [];
-
-    document.getElementById("wins").innerHTML = ("Wins: " + $wins);
-    document.getElementById("guessRemain").innerHTML = ("Ingredients left: " + $guessRemain);document.getElementById("userGuesses").innerHTML = ("Wrong Ingredients: " + usedLetters);
-
-    alert("That's a tasty lookin' drink! Play again?");
-    newCocktail();
-
-  }
+  console.log('newGame start')
 }
 
 // Initial setup
 alert("By clicking 'OK', you confirm you are at least 21 years of age.");
 newGame();
-// Execute function to generate random wordCocktail and insert in html//
-
 newCocktail();
 
 
-
-
-
-
+// AND NOW!!!....the MAIN EVENT....listener.
 //Detects and returns user keystrokes//
 document.onkeyup = function (event) {
 
   // Defines user key input as variable
   var userInput = event.key;
 
-  // Checks if key entered is NOT (!) already included in usedLetters array or current puzzle//
-  if (!usedLetters.includes(userInput) && !correctGuess.includes(userInput)) {
+  if ($guessRemain > 0 && placeholders.join("") !== puzzle) {
 
-    //THEN checks IF keycode is a letter... 
-    if (event.which >= 65 && event.which <= 90) {
+    // Checks if key entered is NOT (!) already included in usedLetters array or current puzzle//
+    if (!usedLetters.includes(userInput) && !correctGuess.includes(userInput)) {
 
-      // THEN checks IF key entered is included in current puzzle...
-      if (puzzle.includes(userInput)) {
-        // Adds guess to array but does not display wrong ingredient
+      //THEN checks IF keycode is a letter... 
+      if (event.which >= 65 && event.which <= 90) {
+
+        // THEN checks IF key entered is included in current puzzle...
+        if (puzzle.includes(userInput)) {
+          // Adds guess to array but does not display as "wrong ingredient"
           correctGuess.push(userInput)
-        // for loop to check...
-        for (i = 0; i < puzzle.length; i++) {
-          // userInput against current puzzle
-          if (puzzle[i] === userInput) {
-            // and set matching user guess equal to placeholder value
-            placeholders[i] = userInput;
-            // and rewrite placeholders with guessed value
-            document.getElementById("cocktailText").innerHTML = placeholders.join("");
-            //and alert user of correct guess in html
-            document.getElementById("gameAlerts").innerHTML = ("Looks good to me. Add it in!");
+          // for loop to check...
+          for (i = 0; i < puzzle.length; i++) {
+            // userInput against current puzzle
+            if (puzzle[i] === userInput) {
+              // and set matching user guess equal to placeholder value
+              placeholders[i] = userInput;
+              // and rewrite placeholders with guessed value
+              document.getElementById("cocktailText").innerHTML = placeholders.join("");
+              //and alert user of correct guess in html
+              document.getElementById("gameAlerts").innerHTML = ("Looks good to me. Add it in!");
+
+              if (placeholders.join("") == puzzle) {
+
+                $wins++;
+            
+                newGame()
+            
+                document.getElementById("wins").innerHTML = ("Wins: " + $wins);
+                newCocktail();
+                alert("Nice job! That's a tasty lookin' drink! Play again?");
+                
+                console.log("foo")
+            
+              }
+
+
+            }
 
           }
 
+        } else {
+          // ...push user keystrokes as values to usedLetters array
+          usedLetters.push(userInput);
+          // and write array and alert to html //
+          document.getElementById("userGuesses").innerHTML = ("Wrong ingredients: " + (usedLetters.join(" ")));
+          document.getElementById("gameAlerts").innerHTML = "Sorry! That's not in this recipe. Try again."
+
+          // and decrease guesses by one and print to html //
+          $guessRemain--;
+          document.getElementById("guessRemain").innerHTML = ("Ingredients left: " + $guessRemain);
+
+          if ($guessRemain === 0 && placeholders.join("") !== puzzle) {
+
+            $losses++;
+          
+            newGame()
+          
+            document.getElementById("losses").innerHTML = ("Losses: " + $losses);
+            newCocktail();
+            alert("You ran out of ingredients. Try again!");
+            
+            console.log("foo")
+        
+          }
+
         }
-
+        // Else alerts user that they must choose a letter //
+      } else {
+        document.getElementById("gameAlerts").innerHTML = "Oops! Not an ingredient. Try adding a letter."
       }
 
-      else {
-        // ...push user keystrokes as values to usedLetters array
-        usedLetters.push(userInput);
-        // and write array and alert to html //
-        document.getElementById("userGuesses").innerHTML = ("Wrong ingredients: " + (usedLetters.join(" ")));
-        document.getElementById("gameAlerts").innerHTML = "Oops! That's not in this recipe. Try again."
-
-        // and decrease guesses by one and alert to html //
-        $guessRemain --;
-        document.getElementById("guessRemain").innerHTML = ("Ingredients left: " + $guessRemain);
-
-      }
-    }
-    // Else alerts user that they must choose a letter //
-    else {
-      document.getElementById("gameAlerts").innerHTML = "Sorry! Not an ingredient. Try adding a letter."
+    } else {
+      document.getElementById("gameAlerts").innerHTML = "We used that ingredient. Try something else!"
     }
 
-  }
+    //log input
+    console.log(userInput);
+    // log array
+    console.log(usedLetters);
 
-  else {
-    document.getElementById("gameAlerts").innerHTML = "We used that ingredient. Try something else!"
   }
-
-  //log input
-  console.log(userInput);
-  // log array
-  console.log(usedLetters);
 
 }
-
-
 
 
 
